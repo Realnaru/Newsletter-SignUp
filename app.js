0c4806b92b05f73e54585026ecb53ed383571c88
Ruslan Opishniak <73569436+Realnaru@users.noreply.github.com>
2021-04-11 16:11:49 +0800

    Update app.js
    Added comments

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-const mailchimp = require("@mailchimp/mailchimp_marketing");
-const express = require("express");
-const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");//require mailchimp module
+const express = require("express");//require express
+const bodyParser = require("body-parser");//require body parser
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public"));//use static folder named "public" to store styles.css
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
-})
+})// sending signup.html 
 
+//mailchimp api config
 mailchimp.setConfig({
   apiKey: "843e7912def9a52f14f32a1e2f790de0-us1",
   server: "us1"
 })
 
-app.post("/", function(req, res) {
 
+app.post("/", function(req, res) {
+  
+      //parsing signup.html to get values of first name, last name, email adress
       const firstName = req.body.firstName;
       const lastName = req.body.lastName;
       const email = req.body.email;
 
-      const listID = "76aae6c4cd";
+      const listID = "76aae6c4cd";//mailchimp list ID
 
+      //object for mailchimp
       const subscribingUser = {
         firstName: firstName,
         lastName: lastName,
         email: email
       };
-
+       
+      // adding users data to mailchimp list
+      // according to mailchimp documentation
       async function run() {
           const response = await mailchimp.lists.addListMember("76aae6c4cd", {
             email_address: subscribingUser.email,
@@ -40,9 +46,9 @@ app.post("/", function(req, res) {
               LNAME: subscribingUser.lastName
             }
           });
-          res.sendFile(__dirname + "/success.html");
+          res.sendFile(__dirname + "/success.html");// if everything is ok send success.html as responce
         }
-        run().catch(e => res.sendFile(__dirname + "/failure.html"));
+        run().catch(e => res.sendFile(__dirname + "/failure.html"));//if not, sending send failure.html
       });
 
       app.post("/failure", (req, res) => {
@@ -50,8 +56,5 @@ app.post("/", function(req, res) {
       })
 
       app.listen(3000, () => console.log("Server is running on port 3000"));
-      // API key
-      // 843e7912def9a52f14f32a1e2f790de0-us1
 
-      // List //
-      // 76aae6c4cd
+      
